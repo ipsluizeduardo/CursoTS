5f179430bbc28bbd3fb211e6c8fce8a8db482d53
ipsluizeduardo <ipsluizeduardo@gmail.com>
2023-07-21 20:18:29 -0300

    Aula

--- build/functions/valor_default.js
+++ build/functions/valor_default.js
@@ -1,1 +1,7 @@
 "use strict";
+// Como deixar um valor default ou opcional.
+function cadastro(email, senha, nome = "Aluno", idade) {
+    let data = { email, senha, nome, idade };
+    console.log(data);
+}
+cadastro("teste@.com", "1234555", "Luiz", 25);
--- src/functions/valor_default.ts
+++ src/functions/valor_default.ts
@@ -1,2 +1,9 @@
 // Como deixar um valor default ou opcional.
 
+ function cadastro(email: string, senha: string, nome = "Aluno", idade?: number): void {
+    let data = { email, senha, nome,idade};
+
+    console.log(data);
+ }
+
+ cadastro("teste@.com", "1234555", "Luiz", 25);
